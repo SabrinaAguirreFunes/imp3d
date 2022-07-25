1c36d686e6c158825cf2d8101d6957ccc7fd1f49
Sabrina Aguirre Funes <sabrinaaguirrefunes@gmail.com>
2022-07-25 20:08:38 -0300

    incorporacion de arrays y objetos para primer pre-entrega

--- index.js
+++ index.js
@@ -1,102 +1,86 @@
-const item1 = 'piezaGrande';
-const item2 = 'piezaMediana';
-const item3 = 'piezaChica';
-const item4 = 'piezaChica';
-const item5 = 'piezaChica';
-const item6 = 'piezaMediana';
-const item7 = 'piezaMediana';
-const item8 = 'piezaChica';
-const item9 = 'piezaGrande';
+const piezaGrande = {
+    tipoPieza: `Grande`,
+    precio: 3000,
+}
+const piezaMediana = {
+    tipoPieza: `Mediana`,
+    precio: 1600,
+}
+const piezaChica = {
+    tipoPieza: `Chica`,
+    precio: 350,
+}
 
+const carrito = [
+    {
+        nombre: `Loona`,
+        tipoPieza: piezaGrande,
+        altura: `17cm`,
+    },
+    {
+        nombre: `Legosi`,
+        tipoPieza: piezaMediana,
+        altura: `12cm`,
+    },
+    {
+        nombre: `Pikachu`,
+        tipoPieza: piezaChica,
+        altura: `5cm`,
+    },
+    {
+        nombre: `Mew`,
+        tipoPieza: piezaChica,
+        altura: `5.5cm`,
+    },
+    {
+        nombre: `Bulbasour`,
+        tipoPieza: piezaChica,
+        altura: `4cm`,
+    },
+    {
+        nombre: `Bakugo`,
+        tipoPieza: piezaMediana,
+        altura: `14cm`,
+    },
+    {
+        nombre: `Ryuk`,
+        tipoPieza: piezaMediana,
+        altura: `13cm`,
+    },
+    {
+        nombre: `Eevee`,
+        tipoPieza: piezaChica,
+        altura: `6cm`,
+    },
+    {
+        nombre: `Batman`,
+        tipoPieza: piezaGrande,
+        altura: `18cm`,
+    },
+]
+
+console.log(carrito)
 
 let cantPiezaGrande = 0;
 let cantPiezaMediana = 0;
 let cantPiezaChica = 0;
 
-if(item1 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item1 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item1 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
 
-if(item2 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item2 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item2 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
-
-if(item3 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item3 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item3 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
-
-if(item4 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item4 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item4 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
-
-if(item5 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item5 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item5 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
-
-if(item6 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item6 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item6 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
-
-if(item7 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item7 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item7 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
-
-if(item8 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item8 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item8 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
-
-if(item9 === 'piezaGrande'){
-    cantPiezaGrande = cantPiezaGrande + 1;
-} else if(item9 === 'piezaMediana'){
-    cantPiezaMediana = cantPiezaMediana + 1;
-} else if(item9 === 'piezaChica'){
-    cantPiezaChica = cantPiezaChica + 1;
-} else 0;
-
-console.log(cantPiezaGrande)
-console.log(cantPiezaMediana)
-console.log(cantPiezaChica)
-
-const precioTotal = function(precioPiezaChica, precioPiezaMediana, precioPiezaGrande) {
-    return cantPiezaChica*precioPiezaChica + cantPiezaMediana*precioPiezaMediana + cantPiezaGrande*precioPiezaGrande
+for (let i = 0; i < carrito.length; i++){
+        if (carrito[i].tipoPieza === piezaGrande){
+            cantPiezaGrande = cantPiezaGrande + 1;
+        } else if (carrito[i].tipoPieza === piezaMediana){
+            cantPiezaMediana = cantPiezaMediana + 1;
+        } else if (carrito[i].tipoPieza === piezaChica){
+            cantPiezaChica = cantPiezaChica + 1;
+        } else 0;
+    
 }
 
-function carrito (precioTotal){
-    return `La suma del carrito hasta ahora es $ ${precioTotal}`
-}
+console.log(`Cantidad de piezas grandes: ${cantPiezaGrande}. Precio unitario: ${piezaGrande.precio}`)
+console.log(`Cantidad de piezas medianas: ${cantPiezaMediana}. Precio unitario: ${piezaMediana.precio}`)
+console.log(`Cantidad de piezas chicas: ${cantPiezaChica}. Precio unitario: ${piezaChica.precio}`)
 
+const precioTotal = cantPiezaGrande*piezaGrande.precio + cantPiezaMediana*piezaMediana.precio + cantPiezaChica*piezaChica.precio
 
-console.log(carrito(precioTotal(350, 1600, 3000)))
-
+console.log (`Precio total: $ ${precioTotal}`)
